9b617a6d30afc6d7a77cfb7b98471ccf05ec1cdb
Marques Jr <studioplaymarques@gmail.com>
2025-10-08 23:03:16 -0300

    Nova Interface Implementada

--- assets/scripts/recompensa.js
+++ assets/scripts/recompensa.js
@@ -1,23 +1,14 @@
-import { GROUP, Slot } from './service/slots.js';
-import { RewardService } from './service/rewardService.js';
-import { MockServerSource } from './service/mockServerSource.js';
-import { LocalJsonSource } from './service/localJsonSource.js';
-
-const source = new MockServerSource();                          //Gera json aleatório
-//const source = new LocalJsonSource('/data/rewards.json');     //Se for utilizar um json local fixo
-//const source = new LocalJsonSource('/api/rewards');          //Quando API estiver pronta
-const service = new RewardService(source);
-await service.load();
+import { Server } from './service/dataInterface.js';
+// @ts-check
 
 const root = document.documentElement;
 const resetTime = document.getElementById("reset-time");
-const wrapperRecompensa = document.getElementById("wrapper-recompensa");
 const cardSlotA = document.getElementById("slot-a");
 const cardSlotB = document.getElementById("slot-b");
 const cardSlotC = document.getElementById("slot-c");
 const btnSelecionarDiario = document.getElementById("btn-recompensa-diaria");
 const btnSelecionarOferta = document.getElementById("btn-oferta-especial");
-let btnSelecionado = GROUP.DAILY;
+let btnSelecionado = Server.Pool.Diario;
 
 const recompensaSwiper = new Swiper('#area-recompensa', {
     direction: 'horizontal',
@@ -48,13 +39,13 @@ cardSlotA.addEventListener('click', () => { revelandoSlot(cardSlotA) });
 cardSlotB.addEventListener('click', () => { revelandoSlot(cardSlotB) });
 cardSlotC.addEventListener('click', () => { revelandoSlot(cardSlotC) });
 
-cardSlotA.addEventListener('animationend', () => { aoRevelarSlot(cardSlotA) });
-cardSlotB.addEventListener('animationend', () => { aoRevelarSlot(cardSlotB) });
-cardSlotC.addEventListener('animationend', () => { aoRevelarSlot(cardSlotC) });
+cardSlotA.addEventListener('animationend', () => { aoRevelarSlot(cardSlotA, Server.Slot.A); });
+cardSlotB.addEventListener('animationend', () => { aoRevelarSlot(cardSlotB, Server.Slot.B); });
+cardSlotC.addEventListener('animationend', () => { aoRevelarSlot(cardSlotC, Server.Slot.C); });
 
-cardSlotA.querySelector("button").addEventListener('click', () => {popupConfirmarCompra(Slot.A)});
-cardSlotB.querySelector("button").addEventListener('click', () => {popupConfirmarCompra(Slot.B)});
-cardSlotC.querySelector("button").addEventListener('click', () => {popupConfirmarCompra(Slot.C)});
+cardSlotA.querySelector("button").addEventListener('click', () => { popupConfirmarCompra(Server.Slot.A) });
+cardSlotB.querySelector("button").addEventListener('click', () => { popupConfirmarCompra(Server.Slot.B) });
+cardSlotC.querySelector("button").addEventListener('click', () => { popupConfirmarCompra(Server.Slot.C) });
 
 // Recompensa diária selecionada ao carregar
 selecionarDiario();
@@ -66,66 +57,77 @@ setInterval(atualizarContagemRegressiva, 1000);
 
 function selecionarDiario() {
 
-    btnSelecionado = GROUP.DAILY;
+    btnSelecionado = Server.Pool.Diario;
     btnSelecionarDiario.classList.remove('btn-liberdade');
     btnSelecionarDiario.classList.add('btn-autoescola');
 
     btnSelecionarOferta.classList.remove('btn-autoescola');
     btnSelecionarOferta.classList.add('btn-liberdade');
 
-    aoSelecionar(Slot.A, Slot.B, Slot.C);
-    removerAguardandoRevelar();
+    aoSelecionar();
 }
 function selecionarOferta() {
 
-    btnSelecionado = GROUP.SPECIAL;
+    btnSelecionado = Server.Pool.Oferta;
     btnSelecionarDiario.classList.remove('btn-autoescola');
     btnSelecionarDiario.classList.add('btn-liberdade');
 
     btnSelecionarOferta.classList.remove('btn-liberdade');
     btnSelecionarOferta.classList.add('btn-autoescola');
 
-    aoSelecionar(Slot.D, Slot.E, Slot.F);
-    removerAguardandoRevelar();
+    aoSelecionar();
 }
 
-function aoSelecionar(slotA, slotB, slotC) {
-    cardSlotA.style.setProperty('--slot-card', service.getCardUrl(slotA));
-    cardSlotB.style.setProperty('--slot-card', service.getCardUrl(slotB));
-    cardSlotC.style.setProperty('--slot-card', service.getCardUrl(slotC));
+function aoSelecionar() {
+    const slotA = Server.card(btnSelecionado, Server.Slot.A);
+    const slotB = Server.card(btnSelecionado, Server.Slot.B);
+    const slotC = Server.card(btnSelecionado, Server.Slot.C);
 
-    cardSlotA.style.setProperty('--slot-silhueta', service.getMaskUrl(slotA));
-    cardSlotB.style.setProperty('--slot-silhueta', service.getMaskUrl(slotB));
-    cardSlotC.style.setProperty('--slot-silhueta', service.getMaskUrl(slotC));
+    cardSlotA.style.setProperty('--slot-card', slotA.qualUrlCarta('../'));
+    cardSlotB.style.setProperty('--slot-card', slotB.qualUrlCarta('../'));
+    cardSlotC.style.setProperty('--slot-card', slotC.qualUrlCarta('../'));
 
-    cardSlotA.style.setProperty('--cor-raridade', service.getRarityColor(slotA));
-    cardSlotB.style.setProperty('--cor-raridade', service.getRarityColor(slotB));
-    cardSlotC.style.setProperty('--cor-raridade', service.getRarityColor(slotC));
+    cardSlotA.style.setProperty('--slot-silhueta', slotA.qualUrlMascara('../'));
+    cardSlotB.style.setProperty('--slot-silhueta', slotB.qualUrlMascara('../'));
+    cardSlotC.style.setProperty('--slot-silhueta', slotC.qualUrlMascara('../'));
 
-    cardSlotA.querySelector("h3").textContent = service.getTitle(slotA);
-    cardSlotA.querySelector("button").textContent = service.getPrettyPrice(slotA);
+    cardSlotA.style.setProperty('--cor-raridade', slotA.qualCorRaridade());
+    cardSlotB.style.setProperty('--cor-raridade', slotB.qualCorRaridade());
+    cardSlotC.style.setProperty('--cor-raridade', slotC.qualCorRaridade());
 
-    cardSlotB.querySelector("h3").textContent = service.getTitle(slotB);
-    cardSlotB.querySelector("button").textContent = service.getPrettyPrice(slotB);
+    cardSlotA.querySelector("h3").textContent = slotA.qualTitulo();
+    cardSlotA.querySelector("button").textContent = slotA.pegarCustoFormatado();
 
-    cardSlotC.querySelector("h3").textContent = service.getTitle(slotC);
-    cardSlotC.querySelector("button").textContent = service.getPrettyPrice(slotC);
+    cardSlotB.querySelector("h3").textContent = slotB.qualTitulo();
+    cardSlotB.querySelector("button").textContent = slotB.pegarCustoFormatado();
 
-    const classAddA = service.getStateClassToAdd(slotA);
-    const classAddB = service.getStateClassToAdd(slotB);
-    const classAddC = service.getStateClassToAdd(slotC);
+    cardSlotC.querySelector("h3").textContent = slotC.qualTitulo();
+    cardSlotC.querySelector("button").textContent = slotC.pegarCustoFormatado();
 
-    const classRemoveA = service.getStateClassToRemove(slotA);
-    const classRemoveB = service.getStateClassToRemove(slotB);
-    const classRemoveC = service.getStateClassToRemove(slotC);
+    gerenciarCardClasses(cardSlotA, slotA);
+    gerenciarCardClasses(cardSlotB, slotB);
+    gerenciarCardClasses(cardSlotC, slotC);
+}
 
-    if (classAddA && classAddA.length) cardSlotA.classList.add(...classAddA);
-    if (classAddB && classAddB.length) cardSlotB.classList.add(...classAddB);
-    if (classAddC && classAddC.length) cardSlotC.classList.add(...classAddC);
-
-    if (classRemoveA && classRemoveA.length) cardSlotA.classList.remove(...classRemoveA);
-    if (classRemoveB && classRemoveB.length) cardSlotB.classList.remove(...classRemoveB);
-    if (classRemoveC && classRemoveC.length) cardSlotC.classList.remove(...classRemoveC);
+/**
+ * 
+ * @param {Element} cardElement 
+ * @param {Server.SlotInterface} cardObject
+ * @returns 
+ */
+function gerenciarCardClasses(cardElement, cardObject) {
+    if (cardObject.foiBloqueado()) {
+        cardElement.classList.remove(...['slot-raridade', 'revelando-card']);
+    } else {
+        if (cardObject.foiRevelado()) {
+            cardElement.classList.add('slot-raridade');
+            cardElement.classList.remove('revelando-card');
+        } else {
+            cardElement.classList.add('revelando-card');
+            cardElement.classList.remove('slot-raridade');
+        }
+    }
+    cardElement.classList.remove('aguardando-revelar');
 }
 
 function revelandoSlot(cardSlot) {
@@ -133,34 +135,35 @@ function revelandoSlot(cardSlot) {
         cardSlot.classList.add('aguardando-revelar');
     }
 }
-function aoRevelarSlot(cardSlot) {
-    cardSlot.classList.remove('aguardando-revelar');
-    cardSlot.classList.remove('revelando-card');
-    cardSlot.classList.add('slot-raridade');
-}
-function removerAguardandoRevelar() {
-    cardSlotA.classList.remove('aguardando-revelar');
-    cardSlotB.classList.remove('aguardando-revelar');
-    cardSlotC.classList.remove('aguardando-revelar');
+
+
+function aoRevelarSlot(cardElement, slot) {
+    cardElement.classList.remove('aguardando-revelar');
+    cardElement.classList.remove('revelando-card');
+    cardElement.classList.add('slot-raridade');
+    Server.card(btnSelecionado, slot).revelar();
+
 }
 
 function popupConfirmarCompra(slot) {
-    const slotSelecionado = slot + 3 * btnSelecionado;
-    if(service.isClaimed(slotSelecionado) || service.isLocked(slotSelecionado)) return;
+
+    const cardObject = Server.card(btnSelecionado, slot);
+
+    if (cardObject.foiComprado() || cardObject.foiBloqueado()) return;
 
     const popupClasses = ['style-liberdade', 'style-rebelde-l', 'style-sombra-adrenalina'];
     const buttonClasses = 'btn-autoescola btn-sombra-adrenalina style-rebelde-r';
 
-    const cardRaridade = service.getPrettyRarity(slotSelecionado);
-    const cardRaridadeCor = service.getRarityColor(slotSelecionado);
-    const cardTitle = `${service.getTitle(slotSelecionado)} 
+    const cardRaridade = cardObject.pegarRaridadeFormatada();
+    const cardRaridadeCor = cardObject.qualCorRaridade();
+    const cardTitle = `${cardObject.qualTitulo()} 
                         <i style="color: ${cardRaridadeCor}; font-size: 0.90rem; background-color: black;">
                             [${cardRaridade}]
                         </i>`;
-    const cardDescription = service.getDescription(slotSelecionado);
-    const cardIcone = service.getIconPath(slotSelecionado, '../assets/');
-    const cardPrice = service.getPrettyPrice(slotSelecionado);
-    
+    const cardDescription = cardObject.qualDescricao();
+    const cardIcone = cardObject.qualCaminhoIcone('../assets/');
+    const cardPrice = cardObject.pegarCustoFormatado();
+
     const contentInflate =
         `<div style="display: flex; flex-direction: column; align-items: center; max-width: 300px;">
 
@@ -176,7 +179,7 @@ function popupConfirmarCompra(slot) {
             <button class="${buttonClasses}">Confirmar</button>
 
         </div>`
-    ;
+        ;
 
     Popup.show({ title: '', content: contentInflate, classes: popupClasses });
 
--- assets/scripts/service/dataInterface.js
+++ assets/scripts/service/dataInterface.js
@@ -1,104 +1,128 @@
-(() => { //Encapsulamento
+//Enum Slot
+const Slot = Object.freeze({ A: 0, B: 1, C: 2 });
+const Pool = Object.freeze({ Diario: 0, Oferta: 1 });
 
-    //Enum Slot
-    const Slot = Object.freeze({ A: 0, B: 1, C: 2 });
-    const Grupo = Object.freeze({ Diario: 0, Oferta: 1 });
+//Origem dos dados
 
-    //Origem dos dados
+const diario_A = {};
+const diario_B = {
+    titulo: 'EMIS Persona', raridade: 'E', custo: 0,
+    descricao: 'Carro clássico inspirado no Opala SS e no Maverick',
+    bloqueado: false, revelado: false, comprado: false,
+    caminhoIcone: 'images/icone-carro.png',
+    caminhoCarta: 'images/card-carro.png',
+    caminhoMascara: 'images/silhueta-carro.png'
+};
+const diario_C = {};
+const oferta_A = {};
+const oferta_B = {
+    titulo: 'EMIS Sparta', raridade: 'L', custo: 250,
+    descricao: 'Carro secular inspirado nos primeiros carros da Ford',
+    bloqueado: false, revelado: false, comprado: false,
+    caminhoIcone: 'images/icone-carro.png',
+    caminhoCarta: 'images/card-carro.png',
+    caminhoMascara: 'images/silhueta-carro.png'
+};
+const oferta_C = {};
 
-    const diario_A = {};
-    const diario_B = {
-        titulo: 'EMIS Persona', raridade: 'E', custo: 0,
-        descricao: 'Carro clássico inspirado no Opala SS e no Maverick',
-        bloqueado: false, revelado: false, comprado: false,
-        caminhoIcone:'images/icone-carro.png',
-        caminhoCarta: 'images/card-carro.png',
-        caminhoMascara: 'images/silhueta-carro.png'
-    };
-    const diario_C = {};
-    const oferta_A = {};
-    const oferta_B = {
-        titulo: 'EMIS Sparta', raridade: 'L', custo: 250,
-        descricao: 'Carro secular inspirado nos primeiros carros da Ford',
-        bloqueado: false, revelado: false, comprado: false,
-        caminhoIcone:'images/icone-carro.png',
-        caminhoCarta: 'images/card-carro.png',
-        caminhoMascara: 'images/silhueta-carro.png'
-    };
-    const oferta_C = {};
+const dados_slot = { [Pool.Diario]: [diario_A, diario_B, diario_C], [Pool.Oferta]: [oferta_A, oferta_B, oferta_C] };
 
-    const dados_slot = { [Grupo.Diario]: [diario_A, diario_B, diario_C], [Grupo.Oferta]: [oferta_A, oferta_B, oferta_C] };
+//Assinatura pública
+export const Server =
+{
+    Slot, Pool,
+    card: buscarSlot,
+    colecao: buscarColecao,
+    progresso: buscarProgresso,
+    inventario: buscarInventario
+};
 
-    //Assinatura pública
-    window.Server =
-    {
-        Slot, Grupo,
-        cardSlot: buscarSlot,
-        colecao: buscarColecao,
-        progresso: buscarProgresso,
-        inventario: buscarInventario
-    };
+/**
+ * @typedef {Object} Server.SlotInterface
+ * @property {() => string} qualTitulo
+ * @property {() => string} qualDescricao
+ * @property {() => boolean} foiBloqueado
+ * @property {() => boolean} foiRevelado
+ * @property {() => boolean} foiComprado
+ * @property {(relativo?: string) => string} qualCaminhoIcone
+ * @property {(relativo?: string) => string} qualCaminhoCarta
+ * @property {(relativo?: string) => string} qualCaminhoMascara
+ * @property {(relativo?: string) => string} qualUrlIcone
+ * @property {(relativo?: string) => string} qualUrlCarta
+ * @property {(relativo?: string) => string} qualUrlMascara
+ * @property {() => ('C'|'R'|'E'|'L'|'')} qualRaridade
+ * @property {() => string} pegarRaridadeFormatada
+ * @property {() => string} qualCorRaridade
+ * @property {() => number} qualCusto
+ * @property {() => string} pegarCustoFormatado
+ * @property {() => boolean} revelar
+ * @property {() => boolean} comprar
+ */
 
-    function buscarSlot(grupo, slot) {
-        //Interface do Slot
-        return Object.freeze({
-            // Leitura
-            qualTitulo() { return dados_slot[grupo]?.[slot]?.titulo ?? 'Slot Bloqueado'; },
-            qualDescricao() { return dados_slot[grupo]?.[slot]?.descricao ?? 'Este slot está bloqueado'; },
+/**
+ * @param {number} pool
+ * @param {number} slot
+ * @returns {Readonly<Server.SlotInterface>}
+ */
+function buscarSlot(pool, slot) {
+    //Interface do Slot
+    return Object.freeze({
+        // Leitura
+        qualTitulo() { return dados_slot[pool]?.[slot]?.titulo ?? 'Slot Bloqueado'; },
+        qualDescricao() { return dados_slot[pool]?.[slot]?.descricao ?? 'Este slot está bloqueado'; },
 
-            foiBloqueado() { return dados_slot[grupo]?.[slot]?.bloqueado ?? true; },
-            foiRevelado() { return dados_slot[grupo]?.[slot]?.revelado ?? false; },
-            foiComprado() { return dados_slot[grupo]?.[slot]?.comprado ?? false; },
+        foiBloqueado() { return dados_slot[pool]?.[slot]?.bloqueado ?? true; },
+        foiRevelado() { return dados_slot[pool]?.[slot]?.revelado ?? false; },
+        foiComprado() { return dados_slot[pool]?.[slot]?.comprado ?? false; },
 
-            qualCaminhoIcone(relativo = './') {
-                return relativo + dados_slot[grupo]?.[slot]?.caminhoIcone ?? relativo + 'images/icone-cadeado.png';
-            },
-            qualCaminhoCarta(relativo = './') {
-                return relativo + dados_slot[grupo]?.[slot]?.caminhoCarta ?? relativo + 'images/silhueta-cadeado.png';
-            },
-            qualCaminhoMascara(relativo = './') {
-                return relativo + dados_slot[grupo]?.[slot]?.caminhoMascara ?? relativo + 'images/silhueta-cadeado.png';
-            },
-            qualUrlIcone(relativo = './') { return `url("${this.qualCaminhoIcone(relativo)}")` },
-            qualUrlCarta(relativo = './') { return `url("${this.qualCaminhoCarta(relativo)}")` },
-            qualUrlMascara(relativo = './') { return `url("${this.qualCaminhoMascara(relativo)}")` },
+        qualCaminhoIcone(relativo = './') {
+            return relativo + (dados_slot[pool]?.[slot]?.caminhoIcone ?? 'images/icone-cadeado.png');
+        },
+        qualCaminhoCarta(relativo = './') {
+            return relativo + (dados_slot[pool]?.[slot]?.caminhoCarta ?? 'images/silhueta-cadeado.png');
+        },
+        qualCaminhoMascara(relativo = './') {
+            return relativo + (dados_slot[pool]?.[slot]?.caminhoMascara ?? 'images/silhueta-cadeado.png');
+        },
+        qualUrlIcone(relativo = './') { return `url("${this.qualCaminhoIcone(relativo)}")` },
+        qualUrlCarta(relativo = './') { return `url("${this.qualCaminhoCarta(relativo)}")` },
+        qualUrlMascara(relativo = './') { return `url("${this.qualCaminhoMascara(relativo)}")` },
 
-            qualRaridade() { return dados_slot[grupo]?.[slot]?.raridade ?? ''; },
-            pegarRaridadeFormatada() {
-                if (this.foiBloqueado()) return '';
-                const raridade = this.qualRaridade();
-                switch (raridade) {
-                    case 'C': return 'Comum';
-                    case 'R': return 'Raro';
-                    case 'E': return 'Épico';
-                    case 'L': return 'Limitado';
-                }
-            },
-            qualCorRaridade() {
-                if (this.foiBloqueado()) return 'var(--cor-predominante)';
-                const raridade = this.qualRaridade();
-                switch (raridade) {
-                    case 'C': return 'var(--cor-liberdade)';
-                    case 'R': return 'var(--cor-autoescola)';
-                    case 'E': return 'var(--cor-adrenalina)';
-                    case 'L': return 'var(--cor-limitada)';
-                }
-            },
-            qualCusto() { return dados_slot[grupo]?.[slot]?.custo ?? 0; },
-            pegarCustoFormatado(){
-                if(this.foiComprado()) return 'Obtido'
-                if(this.qualCusto() > 0) return this.qualCusto();
-                else return 'Grátis';
-            },
+        qualRaridade() { return dados_slot[pool]?.[slot]?.raridade ?? ''; },
+        pegarRaridadeFormatada() {
+            if (this.foiBloqueado()) return '';
+            const raridade = this.qualRaridade();
+            switch (raridade) {
+                case 'C': return 'Comum';
+                case 'R': return 'Raro';
+                case 'E': return 'Épico';
+                case 'L': return 'Limitado';
+            }
+        },
+        qualCorRaridade() {
+            if (this.foiBloqueado()) return 'var(--cor-predominante)';
+            const raridade = this.qualRaridade();
+            switch (raridade) {
+                case 'C': return 'var(--cor-liberdade)';
+                case 'R': return 'var(--cor-autoescola)';
+                case 'E': return 'var(--cor-adrenalina)';
+                case 'L': return 'var(--cor-limitada)';
+            }
+        },
+        qualCusto() { return dados_slot[pool]?.[slot]?.custo ?? 0; },
+        pegarCustoFormatado() {
+            if (this.foiComprado()) return 'Obtido'
+            if (this.qualCusto() > 0) return this.qualCusto();
+            else return 'Grátis';
+        },
 
 
-            // Escrita
-            revelar(){dados_slot[grupo]?.[slot]?.revelado = true; return true;},
-            comprar(){dados_slot[grupo]?.[slot]?.comprado = true; return true;},
+        // Escrita
+        revelar() { dados_slot[pool][slot].revelado = true; return true; },
+        comprar() { dados_slot[pool][slot].comprado = true; return true; },
 
-        });
-    }
-    function buscarColecao() { }
-    function buscarProgresso() { }
-    function buscarInventario() { }
-})();
+    });
+}
+function buscarColecao() { }
+function buscarProgresso() { }
+function buscarInventario() { }
